fd4a9cafc7e43f319a1db2ad0dc012865e63d492
Edwin Villanueva <edwincv0@gmail.com>
2020-01-16 09:37:19 -0800

    Start Work on Spawn Manager.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
-    public GameObject _enemyPrefab;
+    private GameObject _enemyPrefab;
     
     [SerializeField]
     private GameObject _enemyContainer;
+
+    [SerializeField]
+    private GameObject _powerUpPrefab;
+
+    [SerializeField]
+    private GameObject _powerUpContainer;
 
     private bool _stopSpawning = false;
 
     private void Start()
     {
         StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerUpRoutine());
     }
 
     IEnumerator SpawnEnemyRoutine()
@@ -25,6 +32,18 @@ public class SpawnManager : MonoBehaviour
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator SpawnPowerUpRoutine()
+    {
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newPowerUp = Instantiate(_powerUpPrefab, posToSpawn, Quaternion.identity);
+            newPowerUp.transform.parent = _powerUpContainer.transform;
+            float randomSeconds = Random.Range(3f, 7f);
+            yield return new WaitForSeconds(randomSeconds);
         }
     }
 
