569526bb0295d69c1fdcb8af7815508ac6783bce
Edwin <edwincv0@gmail.com>
2020-02-04 19:00:39 -0800

    Add Explosion.

---
+++ Assets/Scripts/Game/Asteroid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotateSpeed = 3.0f;
+
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    private SpawnManager _spawnManager;
+
+    private void Start()
+    {
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        if (_spawnManager == null)
+        {
+            Debug.Log("Warning: The Spawn_Manager is NULL");
+        }
+    }
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, _rotateSpeed, Space.Self);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            _spawnManager.StartSpawning();
+        }
+    }
+}
--- Assets/Scripts/Game/SpawnManager.cs
+++ Assets/Scripts/Game/SpawnManager.cs
@@ -20,12 +20,17 @@ public class SpawnManager : MonoBehaviour
 
     private void Start()
     {
+    }
+
+    public void StartSpawning()
+    {
         StartCoroutine(SpawnEnemyRoutine());
         StartCoroutine(SpawnPowerUpRoutine());
     }
 
     IEnumerator SpawnEnemyRoutine()
     {
+        yield return new WaitForSeconds(3.0f);
         while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
@@ -37,6 +42,7 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnPowerUpRoutine()
     {
+        yield return new WaitForSeconds(3.0f);
         while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
